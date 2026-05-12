ae02b682cf08d9e35c58749e6f173b474d235d28
SALAHEDDINE AIT LHAJ <s.aitlhaj3265@uca.ac.ma>
2026-05-13 00:59:49 +0100

    fix: final build paths

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
-  
+  // Sta3melna ./ bach i-welli i-qra l-assets b-tariqa sahiha f GitHub Pages
+  base: './', 
   build: {
-    // Hadchi k-i-khlli l-build ikoun nqi l GitHub Pages
     outDir: 'dist',
     assetsDir: 'assets',
-  },
-  server: {
-    // Ila knti k-t-khdem b jQuery m3a React, hadchi k-i-sa3ed f l-dev
-    open: true,
   }
 })
